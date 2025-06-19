35fd1531415b70c9e1abcca3809d7f2f476c7402
benjaminlegal-coder <benjaminlegalx@gmail.com>
2025-06-20 00:38:16 +0300

    düzgün şekilde tailwind kurdum vite a test ediyorum

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // React dosyalarınız burada olacak
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
